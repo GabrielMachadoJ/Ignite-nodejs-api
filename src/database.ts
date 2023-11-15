669122ba33be4eeffc453c1b877bea9696560445
Gabriel <gabrielmjj5@gmail.com>
2023-11-15 09:35:41 -0300

    chore: Production config

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,16 @@
+/* eslint-disable prettier/prettier */
 import 'dotenv/config';
 import { knex as setupKnex, Knex } from 'knex';
 import { env } from './env';
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL,
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+        filename: env.DATABASE_URL,
+      }
+      : env.DATABASE_CLIENT,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
